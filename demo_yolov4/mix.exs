eb7083ff5c9d38218c29da3e91e0efd7f306c50f
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2022-12-05 11:46:31 +0900

    added demo_yolov4.

---
+++ demo_yolov4/mix.exs
@@ -0,0 +1,30 @@
+defmodule DemoYOLOv4.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :demo_yolov4,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {DemoYOLOv4.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:onnx_interp, path: ".."},
+      {:cimg, "~> 0.1.14"},
+      {:postdnn, "~> 0.1.4"}
+    ]
+  end
+end
